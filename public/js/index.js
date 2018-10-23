fbc388fa9c102fdd204368cfec0d235ee0e33258
ajay <ajaygupta.9720@gmail.com>
2018-10-23 03:03:54 -0800

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,22 @@
+var socket = io();
+
+        socket.on('connect',function ()  {
+            console.log('connected to server ');
+
+            socket.emit('createMessage',{
+                from:'Ajay',
+                text:'Hello'
+            });
+
+        });
+        socket.on('disconnect',function () {
+            console.log('disconnected from server ');
+        });
+
+        socket.on('newEmail',function(email){
+            console.log('New Email',email);
+        });
+
+        socket.on('newMessage',function(message){
+            console.log('newMessage',message);
+        })
--- server/server.js
+++ server/server.js
@@ -14,9 +14,20 @@ io.on('connection',(socket)=>{
     console.log('New User Connected');
 
 
+
+socket.emit('newMesage',{
+    from:'John',
+    text:'Hi',
+    createdAt:123
+});
+
+socket.on('createMessage',(message) => {
+    console.log('createMessage',message);
+});
+
     socket.on('disconnect',() => {
         console.log('user was disconnected');
-    })
+    });
 });
 
 server.listen(port,() => {
