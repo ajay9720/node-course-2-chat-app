52675c7f58c2e4986ed1bb7a8e39853b72fb2c42
ajay <ajaygupta.9720@gmail.com>
2018-10-25 23:21:31 -0800

    Add css to chat app

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,6 @@
 var expect = require('expect');
-var{generateMessage} = require('./message');
+var{generateMessage,generateLocationMessage} = require('./message');
+
 describe('generateMessage',() => {
     it('should generate correct message object',() =>{
         var from = 'jony';
@@ -7,5 +8,17 @@ describe('generateMessage',() => {
         var message = generateMessage(from,text);
         expect(message.createdAt).toBeA('number');
         expect(message).toInclude({from,text});
+    });
+});
+
+describe('generateLocationMessage',() => {
+    it('should generate correct Location object',() =>{
+        var from = 'deb';
+        var latitude = 15;
+        var longitude = 19;
+        var url = 'https://www.google.com/maps?=15,19';
+        var message = generateLocationMessage(from,latitude,longitude);
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from,url});
     });
 });
