60313c7c0b4f63aeead1aa11302fac86bf8d8421
ajay <ajaygupta.9720@gmail.com>
2018-10-26 22:23:28 -0800

    Scroll to bottom if user is cloose to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 var socket = io();
 
+
+        function scrollToButton(){
+            var messages = jQuery('#messages');
+            var newMessage = messages.children('li:last-child');
+            var clientHeight = messages.prop('clientHeight');
+            var scrollTop = messages.prop('scrollTop');
+            var scrollHeight = messages.prop('scrollHeight');
+            var newMessageHeight = newMessage.innerHeight();
+            var lastMessageHeight = newMessage.prev().innerHeight();
+
+            if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight)
+             messages.scrollTop(scrollHeight);
+        }
+
         socket.on('connect',function ()  {
             console.log('connected to server ')
         });
@@ -18,6 +32,7 @@ var socket = io();
                createdAt:formattedTime
            });
            jQuery('#messages').append(html);
+           scrollToButton();
         });
 
         socket.on('newLocationMessage',function(message){
@@ -29,6 +44,7 @@ var socket = io();
                 createdAt:formattedTime
             });
             jQuery('#messages').append(html);
+            scrollToButton();
         });
 
         
